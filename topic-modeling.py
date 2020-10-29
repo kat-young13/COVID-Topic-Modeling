3f697db4fcd4790e065614a5307ce3eafdadb36f
ppascal2 <ppascal2@gmu.edu>
2020-10-29 17:20:32 -0400

    concat all string columns to new full_text column

--- topic-modeling.py
+++ topic-modeling.py
@@ -2,7 +2,10 @@ from pyspark.sql import SQLContext, SparkSession
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.linalg import Vector, Vectors
 from pyspark.mllib.clustering import LDA, LDAModel
-from pyspark.sql.functions import concat_ws, col, explode, flatten
+from pyspark.sql.functions import concat_ws, col, explode, flatten, collect_list
+from nltk.corpus import stopwords
+import re as re
+from pyspark.ml.feature import CountVectorizer , IDF
 
 conf = SparkConf().setMaster("local").setAppName("TopicModeling")
 sc = SparkContext(conf = conf)
@@ -11,7 +14,9 @@ sqlContext = SQLContext(sc)
 spark = SparkSession.builder.appName("TopicModeling").getOrCreate()
 new_df = sqlContext.read.json("document_parses/test/000a0fc8bbef80410199e690191dc3076a290117.json", multiLine=True)
 print(new_df.printSchema())
+print(new_df.show())
 test = new_df.selectExpr("paper_id", "abstract.text as abs", "body_text.text as body", "metadata.title")
+print(test.printSchema())
 print(test.show())
-temp = test.withColumn("abs", explode(test.abs))
+temp = test.withColumn("full_text", concat_ws(' ',test.abs,test.body,test.title))
 print(temp.show())
