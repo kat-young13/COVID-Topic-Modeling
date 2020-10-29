feb384850b7d282e6da95cb22e7e97020fbf6b58
ppascal2 <ppascal2@gmu.edu>
2020-10-29 16:42:02 -0400

    test

--- topic-modeling.py
+++ topic-modeling.py
@@ -2,13 +2,16 @@ from pyspark.sql import SQLContext, SparkSession
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.linalg import Vector, Vectors
 from pyspark.mllib.clustering import LDA, LDAModel
+from pyspark.sql.functions import concat_ws, col, explode, flatten
 
 conf = SparkConf().setMaster("local").setAppName("TopicModeling")
 sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sc)
 
 spark = SparkSession.builder.appName("TopicModeling").getOrCreate()
-new_df = sqlContext.read.json("archive/document_parses/pdf_json/000a0fc8bbef80410199e690191dc3076a290117.json", multiLine=True)
+new_df = sqlContext.read.json("document_parses/test/000a0fc8bbef80410199e690191dc3076a290117.json", multiLine=True)
 print(new_df.printSchema())
-test = new_df.select("paper_id", "abstract.text", "body_text.text", "metadata.title")
+test = new_df.selectExpr("paper_id", "abstract.text as abs", "body_text.text as body", "metadata.title")
 print(test.show())
+temp = test.withColumn("abs", explode(test.abs))
+print(temp.show())
